f7088495a3951119f2dc6be0933e7b000886dc2f
GabrielBressaglia <gabrielbressaglia@estudante.ufscar.br>
2025-09-02 15:09:45 -0300

    Delete parametros.py

--- parametros.py
+++
@@ -1,9 +0,0 @@
-# Parâmetros do NSGA-II
-num_pop = 40  # Número de indivíduos na população
-num_genes = 30  # Número de genes por indivíduo (variáveis)
-num_obj = 2  # Número de objetivos
-prob_mut = 0.5 # Chance de acontecer a mutacao
-prob_crossover = 1 # Chance de acontecer crossover
-c_parada = 1000
-
-
